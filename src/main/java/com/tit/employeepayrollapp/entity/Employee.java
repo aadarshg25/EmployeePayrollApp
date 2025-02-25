f0e17ef96d4f11dc4716e40e0700ba5822f76575
aadarsh <aadarshg2562003@example.com>
2025-02-25 11:07:31 +0530

    [Aadarsh] Add . UC-4 feature Added Lombok @Slf4j for logging in Employee entity and configured logging settings

--- src/main/java/com/tit/employeepayrollapp/entity/Employee.java
+++ src/main/java/com/tit/employeepayrollapp/entity/Employee.java
@@ -7,16 +7,24 @@ import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Employee {
+    private static final Logger log = LoggerFactory.getLogger(Employee.class);
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private double salary;
     private String department;
+
+    public void logEmployeeCreation() {
+        log.info("Employee created: {}", this);
+    }
 }
